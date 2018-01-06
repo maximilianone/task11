aa9e37a61b8489f846cb45fb260e4933fa17f75b
maximilianone <maximilianone22@gmail.com>
2018-01-06 19:40:58 +0200

    Delete ExistingNumberException.java

--- main/java/controller/exception/ExistingNumberException.java
+++
@@ -1,8 +0,0 @@
-package controller.exception;
-
-public class ExistingNumberException extends RuntimeException{
-    @Override
-    public String getMessage(){
-        return "Task with this number already exist";
-    }
-}
